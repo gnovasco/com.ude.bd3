b7ba7a3fad4d4315c1e073ab249700cb2ddd2710
Nicolás <nicolas.conde@vera.com.uy>
2018-11-12 22:27:26 -0300

    NC.

---
+++ src/com/ude/obligatorio/persistencia/daos/DAORevisionesArchivo.java
@@ -0,0 +1,44 @@
+package com.ude.obligatorio.persistencia.daos;
+
+import com.ude.obligatorio.logica.Revision;
+import com.ude.obligatorio.logica.excepciones.PersistenciaException;
+import com.ude.obligatorio.logica.valueObjects.VORevision;
+
+import java.util.List;
+
+public class DAORevisionesTexto implements IDAORevisiones{
+	public void insBack() {
+		//TODO IMPLLEMTENAR METODO
+	}
+	
+	public int largo() {
+		//TODO IMPLLEMTENAR METODO
+		return -1;
+	}
+	
+	public Revision kesimo() {
+		//TODO IMPLLEMTENAR METODO
+		return null;
+	}
+	
+	public List<VORevision> listarRevisiones() {
+		//TODO IMPLLEMTENAR METODO
+		return null;
+	}
+	
+	public void borrarRevisiones() {
+		//TODO IMPLLEMTENAR METODO
+	}
+
+	@Override
+	public void insBack(Revision rev) throws PersistenciaException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Revision kesimo(int numero) throws PersistenciaException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}	// DAORevisionesTexto
