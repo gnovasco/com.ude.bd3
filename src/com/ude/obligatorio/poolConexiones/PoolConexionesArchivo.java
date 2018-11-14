9c18850d8972e8dbf32adaf7c55703ec96d46167
U-CINF\nconde <nconde@FSO16E0975W073.bps.net>
2018-11-14 11:28:25 -0300

    NC: PoolConexionesArchivo.java

--- src/com/ude/obligatorio/poolConexiones/PoolConexionesArchivo.java
+++ src/com/ude/obligatorio/poolConexiones/PoolConexionesArchivo.java
@@ -7,7 +7,39 @@ import com.ude.obligatorio.poolConexiones.interfaces.IConexion;
 import com.ude.obligatorio.poolConexiones.interfaces.IPoolConexiones;
 
 public class PoolConexionesArchivo implements IPoolConexiones {
+	/*
+	 * Atributos.
+	 */
 	
+	// Bloqueo para mutua exclusion de la estructura.
+	private Lock mapaLock = new ReentrantLock();
+
+	// Array asociativo de nro. de folio a candado.
+	private Map<String, ReadWriteLock> lockMap = new HashMap<String, ReadWriteLock>();
+
+
+	/*
+	 * Metodos.
+	 */
+	
+	/*
+	 * Bloquea la estructura para pedir el candado sobre el folio.
+	 */
+	public IConexion obtenerConexion(boolean modifica) {
+		mapaLock.lock();
+		return null;
+	}	// obtenerConexion
+	
+
+	/*
+	 * Libera la estructura para liberar luego el candado sobre el folio.
+	 */
+	public void liberarConexion(IConexion icon, boolean ok) {
+        mapaLock.unlock();
+	}   // liberarConexion
+
+
+/*	
 	private ReentrantReadWriteLock con;
 	
 	public PoolConexionesArchivo() {
@@ -17,7 +49,7 @@ public class PoolConexionesArchivo implements IPoolConexiones {
 	/*
 	* Solicita una conexi�n al pool. En caso de que todas est�n actualmente en
 	* uso, bloquear� al usuario hasta que otro usuario libere alguna.
-	*/
+	*
 	public IConexion obtenerConexion(boolean modifica) {
         Lock bloq = null;
         
@@ -37,9 +69,10 @@ public class PoolConexionesArchivo implements IPoolConexiones {
 	/*
 	 * Devuelve una conexi�n al pool y avisa a posibles usuarios bloqueados.
 	 *  Siok vale true, har� commit al devolverla, sino har� rollback. 
-	 */
+	 *
 	public void liberarConexion(IConexion icon, boolean ok) {
         icon.unlock();
 	}   // liberarConexion
+*/
 	
 }   // PoolConexionesArchivo
