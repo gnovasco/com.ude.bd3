5e7700628defed0e3740c34cf063c98f39888e4f
gnovasco <gatutus@gmail.com>
2018-11-15 00:41:52 -0300

    servidor rmi - nuevas properties

---
+++ src/com/ude/obligatorio/servidor/Servidor.java
@@ -0,0 +1,45 @@
+package com.ude.obligatorio.servidor;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+import com.ude.obligatorio.configuracion.Configuracion;
+import com.ude.obligatorio.logica.Fachada;
+
+
+
+
+public class Servidor {
+	
+	public static void main (String [] args) {
+		
+		String ipServidor;
+		String puertoServidor;
+
+		try {
+			
+			ipServidor = Configuracion.getProperty("ServidorIp");
+			puertoServidor = Configuracion.getProperty("ServidorPuerto");
+			
+			LocateRegistry.createRegistry(Integer.parseInt(puertoServidor));
+			Fachada fachada = new Fachada();
+			Naming.rebind("//" + ipServidor + ":" + puertoServidor + "/fachada", fachada);
+
+			System.out.println("Corriendo en " + ipServidor + ":" + puertoServidor);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(2);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(3);
+		}
+
+	}
+	
+}
